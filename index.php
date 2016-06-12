1347b155938249f49bcfdf55a02f0a502cce6b74
Siddhant Baviskar <siddhant.baviskar@northumbria.ac.uk>
2016-06-12 16:45:57 +0100

    Added papercard and paperbutton

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 
         <!-- Import elements to be used -->
         <link rel="import" href="bower_components/paper-card/paper-card.html" />
-        <link rel="import" href="/bower_components/paper-header-panel/paper-header-panel.html">
-        <link rel="import" href="/bower_components/paper-toolbar/paper-toolbar.html">
-        <link rel="import" href="/bower_components/iron-flex-layout/iron-flex-layout.html">
-        <link rel="import" href="/bower_components/paper-icon-button/paper-icon-button.html">
+        <link rel="import" href="bower_components/paper-button/paper-button.html" />
+        <link rel="import" href="/bower_components/paper-header-panel/paper-header-panel.html" />
+        <link rel="import" href="/bower_components/paper-toolbar/paper-toolbar.html" />
+        <link rel="import" href="/bower_components/iron-flex-layout/iron-flex-layout.html" />
+        <link rel="import" href="/bower_components/paper-icon-button/paper-icon-button.html" />
         
         <!-- Importing css -->
         <link rel="stylesheet" type="text/css" href="style/main.css" />
@@ -19,5 +20,12 @@
         <paper-toolbar>
             <span class="title">Research</span>
         </paper-toolbar>
+        
+        <paper-card heading="Card Title">
+            <div class="card-content">Some content</div>
+            <div class="card-actions">
+                <paper-button>Some action</paper-button>
+            </div>
+        </paper-card>
     </body>
 </html>
