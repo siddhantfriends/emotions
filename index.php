2b6205430f9be37cc3da8c0d056fc6202d2d16d8
Siddhant Baviskar <siddhant.baviskar@northumbria.ac.uk>
2016-06-12 16:00:49 +0100

    Added required polymer elements and changed index

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
 
         <!-- Import elements to be used -->
         <link rel="import" href="bower_components/paper-card/paper-card.html" />
+        <link rel="import" href="/bower_components/paper-header-panel/paper-header-panel.html">
+        <link rel="import" href="/bower_components/paper-toolbar/paper-toolbar.html">
+        <link rel="import" href="/bower_components/iron-flex-layout/iron-flex-layout.html">
+        <link rel="import" href="/bower_components/paper-icon-button/paper-icon-button.html">
     </head>
     <body>
         <!-- Use elements -->
-        <paper-card heading="Hello World"></paper-card>
+        <paper-toolbar>
+            <span class="title">Research Project</span>
+        </paper-toolbar>
     </body>
 </html>
